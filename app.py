de74438a648bff9f97679e4b545e23562e388f0b
Henry <henryrivera.cs@gmail.com>
2022-04-23 17:59:07 -0400

    crashes stopped but math wrong

--- app.py
+++ app.py
@@ -13,13 +13,15 @@ depth = 0
 vsync = 0
 display = 0
 flags = 0
+donut_size = 100
 
 #colors
 white = (255, 255, 255)
 black = (0, 0, 0)
 class Donut():
     """ Donut class for rendering the object. """
-    def __init__(self):
+    def __init__(self, screen):
+        self.screen = screen
         self.enabled = False
         self.size = 0
         self.pos_x = 0
@@ -30,14 +32,19 @@ class Donut():
     
     def rotate(self):
         """ Rotates the axis to the specified axis position. """
-        print("donut rotating")
+        for T in range(0, 628, 15):
+            cosT, sinT = math.cos(T/100), math.sin(T/100)
+            self.pos_x = self.r2 + (self.r1*cosT)
+            self.pos_y = self.r1 + sinT
+            self.draw(self.size, self.pos_x, self.pos_y)
+        print("donut rotating.")
 
     def draw(self, size, pos_x, pos_y):
         """ Draws the donut wherever pos_x and pos_y are set """
         self.size = size
         self.pos_x = pos_x
         self.pos_y = pos_y
-        pygame.draw.circle(screen, white, (self.pos_x, self.pos_y), 1)
+        pygame.draw.circle(self.screen, white, (self.pos_x, self.pos_y), 1)
 
 class Screen():
     """ instance for initializing basic window stuff. """
@@ -50,7 +57,7 @@ class Screen():
                 depth=depth
                 )
         self.screen.fill(black)
-        self.donut = Donut() #updated when user clicks on screen
+        self.donut = Donut(self.screen) #updated when user clicks on screen
 
     def load_donut(self, pos_x, pos_y):
         """ Loads donut wherever user clicks on the screen. """
@@ -66,8 +73,7 @@ if __name__ == "__main__":
             #place donuts wherever user clicks
             if event.type == pygame.MOUSEBUTTONUP:
                 pos_x, pos_y = pygame.mouse.get_pos()
-                screen.donut.load_donut(pos_x, pos_y)
+                screen.donut.draw(donut_size, pos_x, pos_y)
                 screen.donut.rotate()
             if event.type == pygame.QUIT:
                 pygame.quit()
-        
